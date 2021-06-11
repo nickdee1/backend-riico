e6f20a62738a8b85d7970459c5eca479c26ae5bc
zhuraromdev <zhurarom.dev@gmail.com>
2021-06-11 16:56:06 +0200

    Task2: Db connect

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+client = PyMongo.MongoClient("mongodb+srv://riico_user:riico123@riicocluster.lovtu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.test
 
 
 @app.route('/')
 def hello_world():
+
 	return 'Hello World!'
 
 
