e563eaf7b921f14138eadf9a482921abbfdeb37d
zhuraromdev <zhurarom.dev@gmail.com>
2021-06-11 22:48:22 +0200

    Added models + create script

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask
+from flask import request, jsonify
 import pymongo
 
 app = Flask(__name__)
 
-client = pymongo.MongoClient("mongodb+srv://riico_user:riico123@riicocluster.lovtu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-db = client.test
+# client = pymongo.MongoClient("mongodb+srv://riico_user:riico123@riicocluster.lovtu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+# db = client.test
+#
+# mycol = db["furniture"]
 
 
 @app.route('/')
 def hello_world():
-	# test_data = db.
-	return 'Hello World!'
+	response = {'item': "dwd"}
+	return response
 
 
 if __name__ == '__main__':
