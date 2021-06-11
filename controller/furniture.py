fd66dfb91f300274d7bfcd5a620b9946eb632261
Dvoriadkin, Nikita <dvorinik@fel.cvut.cz>
2021-06-11 19:36:28 +0200

    dev: add dummy controllers

---
+++ controller/furniture.py
@@ -0,0 +1,11 @@
+from flask import request
+
+
+#
+# @app.route()
+# def get_all_furniture():
+#     response = {
+#         'name': 'chair',
+#         'color': 'blue'
+#     }
+#     return response
