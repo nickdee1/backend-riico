ebf11241306404df8b002162d3746798079aac31
Dvoriadkin, Nikita <dvorinik@fel.cvut.cz>
2021-06-12 04:00:43 +0200

    dev: SQL

--- db/create.sql
+++ db/create.sql
@@ -1,6 +1,9 @@
-DROP TABLE IF EXISTS furniture;
 DROP TABLE IF EXISTS component;
-
+DROP TABLE IF EXISTS furniture;
+DROP TABLE IF EXISTS order_item;
+DROP TABLE IF EXISTS payment_order;
+DROP TABLE IF EXISTS cart_item;
+DROP TABLE IF EXISTS cart;
 
 CREATE TABLE furniture (
 	id INTEGER PRIMARY KEY,
@@ -22,12 +25,42 @@ CREATE TABLE component (
 	FOREIGN KEY(furniture_id) REFERENCES furniture(id)
 );
 
--- Insert into component
-insert into Component (id, furniture_id, name, price) values (1, 1, 'Flurazepam Hydrochloride', 1);
-insert into Component (id, furniture_id, name, price) values (2, 1, 'SILICEA', 2);
-insert into Component (id, furniture_id, name, price) values (3, 1, 'ARSENIC TRIOXIDE', 3);
-insert into Component (id, furniture_id, name, price) values (4, 2, 'Methylphenidate Hydrochloride', 4);
-insert into Component (id, furniture_id, name, price) values (5, 2, 'GRANISETRON HYDROCHLORIDE', 5);
+
+CREATE TABLE cart (
+    id INTEGER PRIMARY KEY,
+    total_price float
+);
+
+
+CREATE TABLE cart_item (
+    id INTEGER PRIMARY KEY,
+    price float,
+    cart_id INTEGER,
+    item_id INTEGER,
+    FOREIGN KEY(cart_id) REFERENCES cart(id),
+    FOREIGN KEY(item_id) REFERENCES component(id)
+);
+
+
+CREATE TABLE payment_order (
+    id INTEGER PRIMARY KEY,
+    total_price float,
+    name varchar(64),
+    surname varchar(64),
+    email varchar(64),
+    address varchar(256),
+    telephone varchar(20)
+);
+
+CREATE TABLE order_item (
+    id INTEGER PRIMARY KEY,
+    price float,
+    order_id INTEGER,
+    cart_item_id INTEGER,
+    FOREIGN KEY(order_id) REFERENCES payment_order(id),
+    FOREIGN KEY(cart_item_id) REFERENCES cart_item(id)
+);
+
 
 -- Insert into furniture
 insert into Furniture (id, category, name, color, location, material, width, height) values (1, 'chair', 'Omeprazole', 'Purple', 'Beloyarskiy', 'Vinyl', 1, 1);
@@ -36,3 +69,11 @@ insert into Furniture (id, category, name, color, location, material, width, hei
 insert into Furniture (id, category, name, color, location, material, width, height) values (4, 'chair', 'TRAMETES', 'Teal', 'Hässelby', 'Aluminum', 4, 4);
 insert into Furniture (id, category, name, color, location, material, width, height) values (5, 'chair', 'CEFPODOXIME', 'Khaki', 'Claveria', 'Rubber', 5, 5);
 insert into Furniture (id, category, name, color, location, material, width, height) values (6, 'chairNikita', 'CEFPODONikitaXIME', 'Nikita', 'ClaveriaNikita', 'Nikita', 6, 6);
+
+
+-- Insert into component
+insert into Component (id, furniture_id, name, price) values (1, 1, 'Flurazepam Hydrochloride', 1);
+insert into Component (id, furniture_id, name, price) values (2, 1, 'SILICEA', 2);
+insert into Component (id, furniture_id, name, price) values (3, 1, 'ARSENIC TRIOXIDE', 3);
+insert into Component (id, furniture_id, name, price) values (4, 2, 'Methylphenidate Hydrochloride', 4);
+insert into Component (id, furniture_id, name, price) values (5, 2, 'GRANISETRON HYDROCHLORIDE', 5);
