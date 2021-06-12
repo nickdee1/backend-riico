9d102fc2112d62e376aa565f1f096eddc9e58455
zhuraromdev <zhurarom.dev@gmail.com>
2021-06-12 02:40:25 +0200

    Final version of myqsl db connected with models and controller

--- db/create.sql
+++ db/create.sql
@@ -16,15 +16,11 @@ CREATE TABLE furniture (
 );
 
 
-CREATE TABLE component (id INTEGER PRIMARY KEY, name TEXT UNIQUE NOT NULL, price INTEGER NOT NULL);
-
-
--- Insert into furniture
-insert into Furniture (id, component_id, category, name, color, location, material, width, height) values (1, 1, 'chair', 'Omeprazole', 'Purple', 'Beloyarskiy', 'Vinyl', 1, 1);
-insert into Furniture (id, component_id, category, name, color, location, material, width, height) values (2, 2, 'chair', 'Salicylic Acid', 'Red', 'Sakaidechō', 'Aluminum', 2, 2);
-insert into Furniture (id, component_id, category, name, color, location, material, width, height) values (3, 3, 'chair', 'DEXTROMETHORPHAN', 'Blue', 'Tapada das Mercês', 'Steel', 3, 3);
-insert into Furniture (id, component_id, category, name, color, location, material, width, height) values (4, 4, 'chair', 'TRAMETES', 'Teal', 'Hässelby', 'Aluminum', 4, 4);
-insert into Furniture (id, component_id, category, name, color, location, material, width, height) values (5, 5, 'chair', 'CEFPODOXIME', 'Khaki', 'Claveria', 'Rubber', 5, 5);
+CREATE TABLE component (
+	id INTEGER PRIMARY KEY,
+	name TEXT UNIQUE NOT NULL,
+	price INTEGER NOT NULL
+);
 
 -- Insert into component
 insert into Component (id, name, price) values (1, 'Flurazepam Hydrochloride', 1);
@@ -32,3 +28,11 @@ insert into Component (id, name, price) values (2, 'SILICEA', 2);
 insert into Component (id, name, price) values (3, 'ARSENIC TRIOXIDE', 3);
 insert into Component (id, name, price) values (4, 'Methylphenidate Hydrochloride', 4);
 insert into Component (id, name, price) values (5, 'GRANISETRON HYDROCHLORIDE', 5);
+
+-- Insert into furniture
+insert into Furniture (id, component_id, category, name, color, location, material, width, height) values (1, 1, 'chair', 'Omeprazole', 'Purple', 'Beloyarskiy', 'Vinyl', 1, 1);
+insert into Furniture (id, component_id, category, name, color, location, material, width, height) values (2, 2, 'chair', 'Salicylic Acid', 'Red', 'Sakaidechō', 'Aluminum', 2, 2);
+insert into Furniture (id, component_id, category, name, color, location, material, width, height) values (3, 3, 'chair', 'DEXTROMETHORPHAN', 'Blue', 'Tapada das Mercês', 'Steel', 3, 3);
+insert into Furniture (id, component_id, category, name, color, location, material, width, height) values (4, 4, 'chair', 'TRAMETES', 'Teal', 'Hässelby', 'Aluminum', 4, 4);
+insert into Furniture (id, component_id, category, name, color, location, material, width, height) values (5, 5, 'chair', 'CEFPODOXIME', 'Khaki', 'Claveria', 'Rubber', 5, 5);
+insert into Furniture (id, component_id, category, name, color, location, material, width, height) values (6, 6, 'chairNikita', 'CEFPODONikitaXIME', 'Nikita', 'ClaveriaNikita', 'Nikita', 6, 6);
--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import logging
 
 from flask import request, redirect, render_template, session, url_for, jsonify
 from utils import app, api, cursor
@@ -8,63 +9,64 @@ from utils import app, api, cursor
 
 @app.route("/session", methods=['GET'])
 def session_view():
-    return jsonify({"country_code": session["country_code"],
-                    "phone_number": session["phone_number"]})
+	return jsonify({"country_code": session["country_code"],
+		"phone_number": session["phone_number"]})
 
 
 @app.route("/phone_verification", methods=["GET", "POST"])
 def phone_verification():
-    if request.method == "POST":
-        data = request.get_json()
-        if data:
-            country_code = data["country_code"]
-            phone_number = data["phone_number"]
-        elif request.form.get("country_code") and request.form.get("phone_number"):
-            country_code = request.form.get("country_code")
-            phone_number = request.form.get("phone_number")
-        else:
-            return jsonify({"message": "bad request body"})
+	if request.method == "POST":
+		data = request.get_json()
+		if data:
+			country_code = data["country_code"]
+			phone_number = data["phone_number"]
+		elif request.form.get("country_code") and request.form.get("phone_number"):
+			country_code = request.form.get("country_code")
+			phone_number = request.form.get("phone_number")
+		else:
+			return jsonify({"message": "bad request body"})
 
-        session["country_code"] = country_code
-        session["phone_number"] = phone_number
+		session["country_code"] = country_code
+		session["phone_number"] = phone_number
 
-        api.phones.verification_start(phone_number, country_code, via="sms")
-        return redirect(url_for("verify"))
+		api.phones.verification_start(phone_number, country_code, via="sms")
+		return redirect(url_for("verify"))
 
-    return render_template("phone_verify.html")
+	return render_template("phone_verify.html")
 
 
 @app.route("/verify", methods=["GET", "POST"])
 def verify():
-    if request.method == "POST":
-        data = request.get_json()
-        if data:
-            token = data["token"]
-        elif request.form.get("token"):
-            token = request.form.get("token")
-        else:
-            return jsonify({"message": "bad request body"})
+	if request.method == "POST":
+		data = request.get_json()
+		if data:
+			token = data["token"]
+		elif request.form.get("token"):
+			token = request.form.get("token")
+		else:
+			return jsonify({"message": "bad request body"})
 
-        phone_number = session.get("phone_number")
-        country_code = session.get("country_code")
+		phone_number = session.get("phone_number")
+		country_code = session.get("country_code")
 
-        verification = api.phones.verification_check(phone_number, country_code, token)
+		verification = api.phones.verification_check(phone_number, country_code, token)
 
-        if verification.ok():
-            return jsonify(verification.ok())
+		if verification.ok():
+			return jsonify(verification.ok())
 
-    return render_template("verify.html")
+	return render_template("verify.html")
 
 
 @app.route("/furnitures/<furniture_id>", methods=['GET'])
-def get_furniture(furniture_id):
+def detail_furniture(furniture_id):
+	"""detail of a furniture"""
+
 	cursor.execute(f"SELECT * FROM furniture WHERE id = {furniture_id}")
 	furniture = cursor.fetchone()
 	if not furniture:
-		response = {\
-			'message': 'furniture does not exist'}
+		response = {'message': 'furniture does not exist'}
 		return jsonify(response), 404
-	result = {\
+	data = { \
 		'id': furniture[0],
 		'component_id': furniture[1],
 		'category': furniture[2],
@@ -74,17 +76,21 @@ def get_furniture(furniture_id):
 		'material': furniture[6],
 		'width': furniture[7],
 		'height': furniture[8]}
-	return jsonify(result)
+	ret = jsonify(data)
+
+	logging.info('detail_furniture(furniture_id=%s) -> ret=%s', (furniture_id, ret))
+	return ret
 
 
 @app.route("/furnitures", methods=["GET"])
-def course_list():
+def search_furniture():
+	"""all furnitures"""
+
 	cursor.execute("SELECT * FROM furniture")
 	furnitures = cursor.fetchall()
-
-	dicts = []
+	data = []
 	for furniture in furnitures:
-		dicts.append({ \
+		data.append({ \
 			'id': furniture[0],
 			'component_id': furniture[1],
 			'category': furniture[2],
@@ -94,10 +100,47 @@ def course_list():
 			'material': furniture[6],
 			'width': furniture[7],
 			'height': furniture[8]})
+	ret = jsonify({"furnitures": data})
 
-	return jsonify({
-		"furnitures": dicts})
+	logging.info('search_furniture() -> ret=%s', ret)
+	return ret
 
 
-if __name__ == '__main__':
-	app.run()
+@app.route("/components/<component_id>", methods=['GET'])
+def detail_component(component_id):
+	"""detail of a component"""
+
+	cursor.execute(f"SELECT * FROM component WHERE id = {component_id}")
+	component = cursor.fetchone()
+	if not component:
+		response = {\
+			'message': 'component does not exist'}
+		return jsonify(response), 404
+	result = {\
+		'id': component[0],
+		'name': component[1],
+		'price': component[2]}
+	ret = jsonify(result)
+
+	logging.info('detail_component(component_id=%s) -> ret=%s', (component_id, ret))
+	return ret
+
+
+@app.route("/components", methods=["GET"])
+def search_components():
+	"""all components"""
+
+	cursor.execute("SELECT * FROM component")
+	components = cursor.fetchall()
+
+	dicts = []
+	for component in components:
+		dicts.append({ \
+			'id': component[0],
+			'name': component[1],
+			'price': component[2]})
+	ret = jsonify({"components": dicts})
+
+	logging.info('search_components() -> ret=%s', ret)
+	return ret
+
